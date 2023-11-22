1bfabd4fcd4b7c27aacb7368528432038f60b7ac
sihec <siheca2013@gmail.com>
2023-11-22 14:23:44 -0600

    CERT-0003- swagger documentation
    added swagger documentation into controller

--- authentication/src/main/java/com/siheca/authentication/response/UserResponse.java
+++ authentication/src/main/java/com/siheca/authentication/response/UserResponse.java
@@ -1,6 +1,7 @@
 package com.siheca.authentication.response;
 
 import com.siheca.authentication.entities.User;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +12,9 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 public class UserResponse {
-
+    @ApiModelProperty(notes = "user id", example = "1", required = true)
     private Integer id;
+    @ApiModelProperty(notes = "user name", example = "Joe", required = true)
     private String name;
 
     public UserResponse(User user) {
